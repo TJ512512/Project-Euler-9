ff8eb041b1d74bb27a14c9fdd07e1418557a75a4
TJ512512 <160138699+TJ512512@users.noreply.github.com>
2024-10-20 17:48:41 +0100

    Add files via upload

---
+++ Project Euler 9.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            int answer = 0;
+            for (int i = 0; i < 1; i++)
+            {
+                for (a = 1; a < 1000; a++)
+                {
+                    for (b = 1; b < 1000; b++)
+                    {
+                        for (c = 1; c < 1000; c++)
+                        {
+                            if (a*a+b*b == c*c)
+                            {
+                                //Console.WriteLine("a = " + a);
+                                //Console.WriteLine("b = " + b);
+                                //Console.WriteLine("c = " + c);
+                                //Console.WriteLine();
+                                if (a + b + c == 1000)
+                                {
+                                    answer = a * b * c;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
